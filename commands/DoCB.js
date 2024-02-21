d1095fb5d0d52129c8266de8809180283fbaeb7f
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2024-02-21 11:12:12 +0530

    Update DoCB.js

--- commands/DoCB.js
+++ commands/DoCB.js
@@ -15,8 +15,8 @@
   aliases: 
 CMD*/
 
-var apikey="JNBmE9Rxy8axAwNg" // Enter CB Api Key Here
-var privatekey="1sE6R14tA16LvCUHhzrE4TDkBGBfe89I" // Enter CB Private Key Here
+var apikey="xxx" // Enter CB Api Key Here
+var privatekey="xxx" // Enter CB Private Key Here
 var currency="DOGE" // Enter Currency Here Here
 var amoun="0.001"  // Enter Amo Here
 var amount=amoun*1
