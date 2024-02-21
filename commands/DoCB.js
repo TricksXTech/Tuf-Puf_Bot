6175c6088b2393584545aa91eb1f4bbc3cad3723
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2024-02-21 11:12:48 +0530

    Update DoCB.js

--- commands/DoCB.js
+++ commands/DoCB.js
@@ -15,14 +15,14 @@
   aliases: 
 CMD*/
 
-var apikey="xxx" // Enter CB Api Key Here
-var privatekey="xxx" // Enter CB Private Key Here
+var apikey="xxxx" // Enter CB Api Key Here
+var privatekey="xxxxx" // Enter CB Private Key Here
 var currency="DOGE" // Enter Currency Here Here
 var amoun="0.001"  // Enter Amo Here
 var amount=amoun*1
 var address="techfire166@gmail.com" // Enter Address Here
 HTTP.get({
-url:"https://api.txt-dev.tk/cb/info?key="+apikey+"&pkey="+privatekey+"&curr="+currency+"&amount="+amount+"&address="+address+"",
+url:"https://apilink.com/cb/info?key="+apikey+"&pkey="+privatekey+"&curr="+currency+"&amount="+amount+"&address="+address+"",
 success:'/done'
 })
 
